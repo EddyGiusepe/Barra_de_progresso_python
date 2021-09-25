b1d68cded1dc0141addf8227c2b9d85ba6dc35aa
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-09-24 22:50:53 -0300

    Começando com outro exemplo

--- barra_progresso.py
+++ barra_progresso.py
@@ -17,7 +17,7 @@ print("")
 
 
 print("################################################")
-print(" # Barra de progresso numa lista#")
+print(" # Barra de progresso numa lista #")
 print("################################################")
 
 lista = [1, 2, 3, 10, 15, 20 , -4]
@@ -25,3 +25,34 @@ for i in tqdm(lista):
     time.sleep(1)
 
 print("")
+
+
+
+print("################################################")
+print(" # Atualizando a minha barra de progresso #")
+print("################################################")
+
+
+with tqdm(total=100) as barra_progresso:
+    for i in range(10): # Ver que só vai rodar 10 vezes
+        time.sleep(1)
+        barra_progresso.update(10) #ter cuidado. Porque se você colocar 1 a barra papararia em 10% apenas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
